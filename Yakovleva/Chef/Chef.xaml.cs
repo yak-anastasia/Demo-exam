7aba4fbeb13764a47dc15ac79f42261c0cccde6d
Nastya <Anastasiy.mn579@yandex.ru>
2024-01-27 21:47:22 +0300

    Update button

--- Yakovleva/Chef/Chef.xaml.cs
+++ Yakovleva/Chef/Chef.xaml.cs
@@ -23,5 +23,10 @@ namespace Yakovleva.Chef
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
