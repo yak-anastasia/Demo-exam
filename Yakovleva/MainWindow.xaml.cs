c9c0619cdd5275f84f011501bf0765e38db010b7
Яковлева Анастасия Андреевна <IAkovleva.A.A@corp.kp11.ru>
2024-01-31 14:19:43 +0300

    Admin's functional realization

--- Yakovleva/MainWindow.xaml.cs
+++ Yakovleva/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text;
 using System.Windows;
+using Yakovleva.Models;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -8,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.EntityFrameworkCore;
 
 namespace Yakovleva
 {
@@ -19,6 +21,46 @@ namespace Yakovleva
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Получаем введенные данные из TextBox и PasswordBox
+            string username = UsernameTextBox.Text;
+            string password = PasswordBox.Password;
+
+            using (var context = new YakovlevaContext())
+            {
+                var user = context.Users.Include(u => u.Role).FirstOrDefault(u => u.Login == username && u.Password == password);
+                if (user != null)
+                {
+                    switch (user.Role.Name) 
+                    {
+                        case "Админ":
+                            Views.Admin.AdminWindow adminWindow = new Views.Admin.AdminWindow();
+                            adminWindow.Show();
+                            this.Close();
+                            break;
+                        case "Официант":
+                            Views.Waiter.Waiter waiterWindow = new Views.Waiter.Waiter();
+                            waiterWindow.Show();
+                            this.Close();
+                            break;
+                        case "Повар":
+                            Views.Chef.Chef chefWindow = new Views.Chef.Chef();
+                            chefWindow.Show();
+                            this.Close();
+                            break;
+                        default:
+                            MessageBox.Show("Неизвестная роль пользователя");
+                            break;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Неверный логин или пароль");
+                }
+            }
         }
     }
 }
---
+++ Yakovleva/Views/Chef/Chef.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Yakovleva.Views.Chef
+{
+    /// <summary>
+    /// Interaction logic for Chef.xaml
+    /// </summary>
+    public partial class Chef : Window
+    {
+        public Chef()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
